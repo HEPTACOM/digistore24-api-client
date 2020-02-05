e91575d9cec64154b080ada4a84997632266a41e
Joshua Behrens <behrens@heptacom.de>
2020-02-05 16:42:06 +0100

    Add globalSettings

--- jane/Client.php
+++ jane/Client.php
@@ -9,6 +9,22 @@ class Client extends \Jane\OpenApiRuntime\Client\Psr18Client
      *
      *     @var string $operator
      *     @var string $language de, en
+     * }
+     *
+     * @param string $fetch Fetch mode to use (can be OBJECT or RESPONSE)
+     *
+     * @return \Heptacom\DigiStore24\Model\ErrorResponse|\Psr\Http\Message\ResponseInterface|null
+     */
+    public function globalSettings(string $apiToken, array $queryParameters = [], string $fetch = self::FETCH_OBJECT)
+    {
+        return $this->executePsr7Endpoint(new \Heptacom\DigiStore24\Endpoint\GlobalSettings($apiToken, $queryParameters), $fetch);
+    }
+
+    /**
+     * @param array $queryParameters {
+     *
+     *     @var string $operator
+     *     @var string $language de, en
      *     @var array $search
      * }
      *
